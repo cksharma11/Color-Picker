8be5b7161eca1d1caf0a88ba3e78e050ad0f60ba
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-08-25 16:32:10 +0530

    Updates

---
+++ precache-manifest.758d2921c10a1e3001f9ac24d6b50886.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5b602a6c6179db81edd80b39ba0a01",
+    "url": "/color-picker/index.html"
+  },
+  {
+    "revision": "b038ea5d6df9bb4e8761",
+    "url": "/color-picker/static/css/main.1551b835.chunk.css"
+  },
+  {
+    "revision": "309083c019dde3b48d6f",
+    "url": "/color-picker/static/js/2.82667d99.chunk.js"
+  },
+  {
+    "revision": "b038ea5d6df9bb4e8761",
+    "url": "/color-picker/static/js/main.52e4affe.chunk.js"
+  },
+  {
+    "revision": "fd86e74ea6ee3c43f9ec",
+    "url": "/color-picker/static/js/runtime~main.7413dd3d.js"
+  }
+]);
