77df33a13967830648ce82211112df662cbd8a38
Chandan Kumar <cksharma122@gmail.com>
2021-01-21 23:49:24 +0530

    On-hover will show hexcode

--- src/components/ColorBox/ColorBox.js
+++ src/components/ColorBox/ColorBox.js
@@ -1,23 +1,27 @@
-import React, { useState } from "react";
-import Clipboard from "react-clipboard.js";
+import React, { useState } from 'react';
+import Clipboard from 'react-clipboard.js';
 
 const ColorBox = ({ hexCode, colorName }) => {
   const [status, setStatus] = useState(colorName);
+  const [activeClass, setActiveClass] = useState('div-colorbox');
   const setCopySuccess = () => {
-    setStatus("Copied");
+    setStatus('Copied');
+    setActiveClass('copied');
     setTimeout(() => {
       setStatus(colorName);
+      setActiveClass('div-colorbox');
     }, 1000);
   };
 
   return (
     <Clipboard
       data-clipboard-text={hexCode}
-      style={{ background: "none", border: "none", fontFamily: "inherit" }}
+      style={{ background: 'none', border: 'none', fontFamily: 'inherit' }}
     >
       <div
         id={hexCode}
-        className="div-colorbox"
+        className={activeClass}
+        data-hover={hexCode}
         style={{ background: `${hexCode}` }}
         onClick={setCopySuccess}
       >
